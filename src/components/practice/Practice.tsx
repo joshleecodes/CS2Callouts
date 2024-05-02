49b519a7f21adf1062b17c834d6024e22f6bc111
Josh Lee <joshlee.dev@gmail.com>
2024-05-02 11:35:53 +0100

    Practice component interval implemented correctly, count limit implemented

--- src/components/practice/Practice.tsx
+++ src/components/practice/Practice.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect} from 'react';
 //import { random } from 'lodash';
 
-
 interface PracticeProps {
   controlImage: string;
   imageSet: string[];
@@ -13,53 +12,40 @@ const Practice = ({
   imageSet,
   countSelection
 }: PracticeProps) => {
-  const [currentImage, setCurrentImage] = useState<string>(controlImage);
-  const [intervalId, setIntervalId] = useState<number>(0);
+  const [counter, setCounter] = useState(0);
+  const [currentImage, setCurrentImage] = useState(controlImage);
+  const [timeoutId, setTimeoutId] = useState(0);
 
-  const randomizeImage = () => {
+  const randomTimeout = () => {
+    return Math.floor(Math.random() * (2500 - 1000) + 1000); // Random timeout between 1 to 2.5 seconds
+  };
+
+  const getRandomImage = () => {
     const randomIndex = Math.floor(Math.random() * imageSet.length);
     return imageSet[randomIndex];
-  }
+  };
 
-  const displayControlImage = () => {
-    setCurrentImage(controlImage)
-      pauseInterval();
+  const handleImageChange = () => {
+    clearTimeout(timeoutId);
+    setCurrentImage(controlImage);
+    setTimeout(() => {
+      setCurrentImage(getRandomImage());
+    }, randomTimeout());
+  
   };
 
   useEffect(() => {
-    clearInterval(intervalId);
-  }, [currentImage]);
+    if (counter < countSelection) {
+      handleImageChange();
+    } else console.log('counter reached, show stats');
+  }, [counter]);
 
   const handleClick = () => {
-    if(currentImage != controlImage){
-      displayControlImage();
-      resumeInterval();
+    if (currentImage !== controlImage) {
+      setCounter(counter + 1);
+      console.log(counter);
     }
-  }
-
-  const startInterval = () => {
-    // Display random image after random time between 1 to 2.5 seconds
-    const randomInterval = Math.floor(Math.random() * (2500 - 1000) + 1000)
-    console.log(randomInterval);
-    
-    setTimeout(() => {
-      setCurrentImage(randomizeImage()); 
-        pauseInterval();
-    }, randomInterval);
-
-  }
-
-  const pauseInterval = () => {
-    clearInterval(intervalId); // Pause interval
-  }
-
-  const resumeInterval = () => {
-    setTimeout(() => {
-      startInterval(); // Resume interval after 3 seconds
-    }, 0);
-  }
-
-  startInterval();
+  };
 
   return (
     <div>
@@ -69,8 +55,7 @@ const Practice = ({
         onClick={handleClick}   
       />
     </div>
-  )
-  
-}
+  );
+};
 
 export default Practice;
